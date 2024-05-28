9f3b25c1a15e2b2e23f02900e653a34249fc6571
fabiosrocha2019 <fabinho.s.rocha@hotmail.com>
2024-05-28 20:12:49 -0300

    dev endpoint

--- src/main/java/com/batalhanaval/controller/UserController.java
+++ src/main/java/com/batalhanaval/controller/UserController.java
@@ -117,5 +117,33 @@ public class UserController {
         return ResponseEntity.ok(user);
     }
 
+    @PutMapping("{userId}/alterar-diamante")
+    @CrossOrigin
+    public ResponseEntity<User> alterarDiamante(@RequestBody String novoDiamante, @PathVariable Long userId) {
+        User user = this.userService.getUser(userId);
 
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        user.setDiamante(novoDiamante);
+        user = this.userService.saveUser(user);
+
+        return ResponseEntity.ok(user);
+    }
+
+    @PutMapping("{userId}/alterar-moeda")
+    @CrossOrigin
+    public ResponseEntity<User> alterarMoeda(@RequestBody String novaMoeda, @PathVariable Long userId) {
+        User user = this.userService.getUser(userId);
+
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        user.setMoeda(novaMoeda);
+        user = this.userService.saveUser(user);
+
+        return ResponseEntity.ok(user);
+    }
 }
